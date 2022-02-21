257574489e8fd742bfdb7d4e30b802f8c119b738
aclelandx <95543064+aclelandx@users.noreply.github.com>
2022-02-20 23:50:02 -0500

    Added in the logic for the licenses to be selected and have the appropriate information being added to the page

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# README-Generator-NodeJS-Adam-Cleland
-As a portfolio Assignment for the OSU Coding Boot Camp we are tasked with creating a professional README Generator that allows the user to input their information onto a new MD file labeled Readme
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+# The Testing of the generator
+
+The hole purpose of me writing this right now to to make sure that THIS information is being put in the correct location for the new MD file that is goiong to be generated at the end of the script
+
+## Table of contents
+---
+- **Installation Instructions**
+- **Usage Information**
+- **Languages Utilized**
+- **Contributes**
+- **Questions**
+- **License**
+
+## Languages Utilized
+---
+
+* js
+* css
+* html5
+
+## Questions
+---
+
+ ### For Any Questions about the Application feel free to reach out to me on:
+
+Github:[aclelandx](https://github.com/aclelandx)
+
+Email:<aclelandx@gmail.com>
+
+## License
+---
+Copyright [2022] [Adam Cleland]
+
+        Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+        
+        The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+        
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ index.js
@@ -0,0 +1,177 @@
+const inquirer = require(`inquirer`);
+const fs = require(`fs`);
+
+inquirer
+    .prompt([
+        {
+            type: `input`,
+            message: `What is Your GitHub Username?\n`,
+            name: `gitUsername`,
+        },
+
+        {
+            type: `input`,
+            message: `What is your Email Address?\n`,
+            name: `emailAddress`,
+        },
+
+        {
+            type: `input`,
+            message: `What is the name of your Repo?\n`,
+            name: `repoName`,
+        },
+
+        {
+            type: `input`,
+            message: `Please write a short Description about your application.\n`,
+            name: `repoDesc`,
+        },
+
+        {
+            type: `input`,
+            message: `What languages were utilized with this application? (Separate Each By a Comma)\n`,
+            name: `languages`,
+        },
+
+        {
+            type: `list`,
+            name: `license`,
+            message: `Select which license you would like to have added`,
+            choices: [`MIT`, `Apache`, `Unlicense`],
+            default: `MIT`,
+        },
+
+        {
+            type: `input`,
+            name: `owner`,
+            message: `What name would you like the License to be under?`,
+        }
+    ])
+    .then((userInput) => {
+        let outputInfo =
+            `
+${licenseBadgePop(userInput.license)}
+# ${userInput.repoName}
+
+${userInput.repoDesc}
+
+## Table of contents
+---
+- **Installation Instructions**
+- **Usage Information**
+- **Languages Utilized**
+- **Contributes**
+- **Questions**
+- **License**
+
+## Languages Utilized
+---
+${mdList(userInput.languages, `,`)}
+
+## Questions
+---
+\n ### For Any Questions about the Application feel free to reach out to me on:
+\nGithub:[${userInput.gitUsername}](https://github.com/${userInput.gitUsername})
+\nEmail:<${userInput.emailAddress}>
+
+## License
+---
+${licenseInformationPop(userInput.license, userInput.owner)}`
+
+            ;
+
+        fs.writeFile(`README.md`, outputInfo, (err) =>
+            err ? console.error(err) : console.log(`File has been created, Check your Local Files`)
+        );
+    });
+
+// Splits the information in order to make a list and removes the comma from the array map for the print out.
+
+function mdList(listItem, separator) {
+    let splitter = listItem.split(separator);
+    let listArray = [splitter].flat(2).map(self => `\n* ` + self.trim());
+    let output = String(listArray).replace(/,/g, ``);
+    return output;
+};
+
+function licenseInformationPop(selection, CRName) {
+    let grabDate = new Date();
+    let year = grabDate.getFullYear();
+    let selectedLicense;
+
+    if (selection === `MIT`) {
+        selectedLicense =
+            `Copyright [${year}] [${CRName}]
+
+        Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+        
+        The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+        
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`;
+        return selectedLicense;
+
+    } else if (selection === `Apache`) {
+        selectedLicense = `Copyright [${year}] [${CRName}]
+
+        Licensed under the Apache License, Version 2.0 (the "License");
+        you may not use this file except in compliance with the License.
+        You may obtain a copy of the License at
+        
+            http://www.apache.org/licenses/LICENSE-2.0
+        
+        Unless required by applicable law or agreed to in writing, software
+        distributed under the License is distributed on an "AS IS" BASIS,
+        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        See the License for the specific language governing permissions and
+        limitations under the License.`;
+        return selectedLicense;
+    } else if (selection === `Unlicense`) {
+        selectedLicense =
+            `This is free and unencumbered software released into the public domain.
+
+        Anyone is free to copy, modify, publish, use, compile, sell, or
+        distribute this software, either in source code form or as a compiled
+        binary, for any purpose, commercial or non-commercial, and by any
+        means.
+        
+        In jurisdictions that recognize copyright laws, the author or authors
+        of this software dedicate any and all copyright interest in the
+        software to the public domain. We make this dedication for the benefit
+        of the public at large and to the detriment of our heirs and
+        successors. We intend this dedication to be an overt act of
+        relinquishment in perpetuity of all present and future rights to this
+        software under copyright law.
+        
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+        EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+        MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+        IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+        OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+        ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+        OTHER DEALINGS IN THE SOFTWARE.
+        
+        For more information, please refer to <http://unlicense.org/>`;
+        return selectedLicense;
+    }
+    return selectedLicense;
+}
+
+function licenseBadgePop(selection) {
+    let badgeLink;
+    if (selection === `MIT`) {
+        badgeLink = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+    } else if (selection === `Apache`) {
+        badgeLink = `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+    } else if (selection === `Unlicense`) {
+        badgeLink = `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`
+    }
+    return badgeLink;
+}
+
+// i need to make the title, description, table of contents installation instructions, usage license contribution guidelines tests and questions
+
+// section entitles questions
+
+// ask for github username, ask for email address, ask which License they would like to use.
+
+// i need to have a badge added to the top of the repository of the license type that ill be chosen by the user accompanied by the information about the license 
